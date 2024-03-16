17a8c5c123bfcfe2167646d48a26b311bdf35496
neiknguyen040402 <kiennguyengtglhd@gmail.com>
2024-03-16 16:11:51 +0700

    all

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Ngọc à!',
+    introDesc: `Đừng có ấn "Dạ" nha `,
+    btnIntro: 'Dạ 🙈',
+    title: `Phải chăng anh đã say ngay từ lúc thấy 
+    nụ cười ấy 😝`,
+    desc: 'Bắt nạt với ăn vạ anh ít thui nhé =))',
+    btnYes: 'Okiiiiiii',
+    btnNo: 'Không, Anh trai à hí :>',
+    question:'Muốn nhắn gì cho anh không :>>',
+    btnReply: 'Gửi cho anh ở đây :>',
+    reply: 'Thật ra em thích ăn vạ anh cũng có lý do í ...',
+    mess: 'Anh biết mà 🥰',
+    messDesc: '',
+    btnAccept: '😀',
+    messLink: 'https://m.me/neikonguci.de.44' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
